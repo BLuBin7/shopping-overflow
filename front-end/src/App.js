73af318b24e91b639b3c819c2c40705f5f31acda
BLuBin7 <builuongbinh7@gmail.com>
2023-07-08 23:44:24 +0700

    add Home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 // add modules bootstrap
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+// test 
+import Login from './Login.js';
+
 
 function App() {
   return (
     <div className="App">
+      {/* test */}
+      <Login/>
       
+      <Home />
       <Navbar />
     </div>
   );
