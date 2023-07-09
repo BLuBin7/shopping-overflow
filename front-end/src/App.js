3ce43730d4e4885e10f3464f40c7ab790e660814
BLuBin7 <builuongbinh7@gmail.com>
2023-07-09 22:37:49 +0700

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,27 @@
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 // add modules bootstrap
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './layout/Navbar';
-import Home from './pages/Home';
-// test 
-import Login from './Login.js';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+// test
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      {/* test */}
-      <Login/>
-      
-      <Home />
-      <Navbar />
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Login</p>
+        </header>
+        {/* test */}
+        <Login />
+
+        <Navbar />
+        <Home />
+      </div>
+    </>
   );
 }
 
