f2d2b0fda439547743fad4ba80e11588abd45ff5
BLuBin7 <builuongbinh7@gmail.com>
2023-07-19 22:25:17 +0700

    update fetch api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,23 +6,37 @@ import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
 // test
 import Login from "./pages/Login";
+import Appbar from './component/Appbar'
+import User from './component/User'
+// function App() {
+//   return (
+//     <>
+//       <div className="App">
+        
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>Login</p>
+//         </header>
+//         {/* test */}
+//         <Login />
+
+//         <Navbar />
+//         <Home />
+  
+//       </div>
+//     </>
+//   );
+// }
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Login</p>
-        </header>
-        {/* test */}
-        <Login />
-
-        <Navbar />
-        <Home />
-      </div>
-    </>
+    <div className="App">
+    <Appbar/>
+    <User/>
+   
+    </div>
   );
 }
+
 
 export default App;
