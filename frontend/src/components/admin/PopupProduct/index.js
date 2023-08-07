4fbd8d04ba0c1986b9a46e65c701b402e76aa695
BLuBin7 <builuongbinh7@gmail.com>
2023-08-08 00:41:26 +0700

    update

--- frontend/src/components/admin/PopupProduct/index.js
+++ frontend/src/components/admin/PopupProduct/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './style.scss';
-const ProductPopup = ({ products, onClose }) => {
+
+const ProductPopup = ({ products, onClose ,searchKey,fetchProducts}) => {
+
+  const [popupSearchKey, setPopupSearchKey] = useState('');
+
+  const handlePopupSearch = () => {
+    fetchProducts(0, popupSearchKey); // Assuming the first page for popup search
+  };
+
   return (
     <div className="product-popup">
-      <button onClick={onClose}>Close</button>
+       <input
+        type="text"
+        value={popupSearchKey}
+        onChange={(e) => setPopupSearchKey(e.target.value)}
+        placeholder="Search products in popup"
+      />
+      <button onClick={handlePopupSearch}>Search</button>
+      <p>Search Key: {searchKey}</p>
+      {products.length === 0 ? (
+        <p>No products to display</p>
+      ) :  (
       <ul className="products">
+        <p>sak</p>
         {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
+          <div key={product.productId}>
+          <h3>{product.productName}</h3>
+          <hr />
+        </div>
+         ))}
+        <p>nmom</p>
       </ul>
+      )}
     </div>
   );
 };
