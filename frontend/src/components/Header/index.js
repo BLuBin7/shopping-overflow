f3c2d994acb0cbf10f1207e12ccda78866a7ab06
BLuBin7 <builuongbinh7@gmail.com>
2023-09-14 18:45:12 +0700

    update Header use Bootstrap

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import "./style.scss";
-import { Link, NavLink, Router, Route, useNavigate  } from "react-router-dom";
+import { Link, NavLink, Router, Route, useNavigate } from "react-router-dom";
 import Login from "../Signup";
 import AllProducts from "../admin/allProducts";
 const Header = () => {
   const [searchKey, setSearchKey] = useState("");
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
-
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,30 +15,77 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
+    <div style={{ backgroundColor: "orange" }}>
       <h1>My Website</h1>
-      <nav className="nav">
+      <div className="row">
+        <div className="col-md-6">
+          <nav className="nav">
+            <ul className="nav ">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/about"
+                >
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="col-md-6">
+          <nav className="nav justify-content-end">
+            <ul className="nav ">
+              <li className="nav-item">
+                <NavLink to="/signup" className="ms-auto">
+                  Sign up
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <a href="/cart" className="ms-2">
+                  Cart
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <nav className="nav justify-content-center">
         <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/contact">Product</a>
-          </li>
-          <li>
-            <NavLink to="/signup">Sign up</NavLink>
-          </li>
-          <li>
-            <a href="/cart">Cart</a>
-          </li>
-          <li>
-            <form onSubmit={handleSearchSubmit}>
+          <form className="d-flex" onSubmit={handleSearchSubmit}>
+            <input
+              className="form-control me-2"
+              type="text"
+              aria-label="Search"
+              name="searchKey"
+              value={searchKey}
+              onChange={(e) => setSearchKey(e.target.value)}
+              placeholder="Search products..."
+              style={{ width: "700px" }}
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </ul>
+
+        {/* <form onSubmit={handleSearchSubmit}>
               <input
                 type="text"
                 name="searchKey"
@@ -48,11 +94,9 @@ const Header = () => {
                 placeholder="Search products..."
               />
               <button type="submit">Search</button>
-            </form>
-          </li>
-        </ul>
+            </form> */}
       </nav>
-    </header>
+    </div>
   );
 };
 
