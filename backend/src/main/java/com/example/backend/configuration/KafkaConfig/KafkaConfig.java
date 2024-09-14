54e620ba6626223bb4dd0d7b84fac1578e6fffaf
BLuBin7 <builuongbinh7@gmail.com>
2024-09-15 01:15:20 +0700

    build(deps): update frontend dependencies for security
    Updated several frontend dependencies, including React and Redux, to address security vulnerabilities. Ensure compatibility with current codebase.
    
    See npm audit for details.

---
+++ backend/src/main/java/com/example/backend/configuration/KafkaConfig/KafkaConfig.java
@@ -0,0 +1,2 @@
+package com.example.backend.configuration.KafkaConfig;public class KafkaConfig {
+}
---
+++ backend/src/main/java/com/example/backend/configuration/RedisConfig/RedisConfig.java
@@ -0,0 +1,2 @@
+package com.example.backend.configuration.RedisConfig;public class RedisConfig {
+}
